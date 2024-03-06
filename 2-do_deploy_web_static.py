04ccd78a90ecc0eece9fa614cca196fdd077d677
Dayvid0063 <orjidavid63@gmail.com>
2024-03-06 18:25:29 +0100

    new test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,19 +17,19 @@ def do_deploy(archive_path):
         return False
 
     filename = os.path.basename(archive_path)
-    dir_name = filename.replace(".tgz", "")
-    dir_path = "/data/web_static/releases/{}/".format(dir_name)
+    folder_name = filename.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
     res = False
 
     try:
         put(archive_path, '/tmp/')
-        run("mkdir -p {}".format(dir_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, dir_path))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(filename, folder_path))
         run("rm -rf /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(dir_path, dir_path))
-        run("rm -rf {}web_static".format(dir_path))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(dir_path))
+        run("ln -s {} /data/web_static/current".format(folder_path))
         print('Deployed!')
         res = True
     except Exception:
