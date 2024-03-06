23b0bfd65cadd13c85e4bed227298af7ffc6690a
Dayvid0063 <orjidavid63@gmail.com>
2024-03-06 18:33:03 +0100

    testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,10 +2,33 @@
 """Distributes an archive to my web servers"""
 
 
-from fabric.api import env, put, run
+from datetime import datetime
+from fabric.api import env, put, run, local
 import os
 
 env.hosts = ['100.24.244.220', '34.239.250.31']
+
+
+def do_pack():
+    """Creates a .tgz archive"""
+    dt_stamp = datetime.utcnow()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt_stamp.year,
+        dt_stamp.month,
+        dt_stamp.day,
+        dt_stamp.hour,
+        dt_stamp.minute,
+        dt_stamp.second
+    )
+
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+
+    res = local("tar -cvzf {} web_static".format(output))
+    if res.failed:
+        return None
+
+    return output
 
 
 def do_deploy(archive_path):
@@ -16,22 +39,22 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
+    put(archive_path, '/tmp/')
+
     filename = os.path.basename(archive_path)
-    folder_name = filename.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    res = False
+    dir_name = filename.replace(".tgz", "")
+    dir_path = "/data/web_static/releases/{}/".format(dir_name)
 
     try:
-        put(archive_path, '/tmp/')
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, folder_path))
+        run("mkdir -p {}".format(dir_path))
+        run("tar -xzf /tmp/{} -C {}".format(filename, dir_path))
         run("rm -rf /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
+        run("mv {}web_static/* {}".format(dir_path, dir_path))
+        run("rm -rf {}web_static".format(dir_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        run("ln -s {} /data/web_static/current".format(dir_path))
         print('Deployed!')
-        res = True
-    except Exception:
-        res = False
-    return res
+        return True
+    except Exception as e:
+        print('Deployment failed:', str(e))
+        return False
