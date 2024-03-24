b35dc8f54cb7b823beba43d751c520c938294619
Dayvid0063 <orjidavid63@gmail.com>
2024-03-24 12:50:38 +0100

    new

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -12,15 +13,16 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays states list sorted by name"""
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State").values()
+    sorted_state = sorted_state(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", sorted_state=sorted_state)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
     """Closes the session after each request"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
