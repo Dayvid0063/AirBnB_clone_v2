c62e65a288dae250169a80a124a73131b5009842
Dayvid0063 <orjidavid63@gmail.com>
2024-03-25 04:18:46 +0100

    completed

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,27 +4,45 @@
 
 from flask import Flask
 from flask import render_template
-from models import *
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
     """Removes the current Session"""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities ls in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    '''States page'''
+    states = None
+    state = None
+    all_states = list(storage.all(State).values())
+    case = 404
+    if id is not None:
+        res = list(filter(lambda x: x.id == id, all_states))
+        if len(res) > 0:
+            state = res[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 2
+    else:
+        states = all_states
+        for state in states:
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
+    ctxt = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **ctxt)
 
 
 if __name__ == '__main__':
